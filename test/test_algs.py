6a1efc42d32a12c1e5311cf4fd839b44f45b7ba7
Andrew Kung <andrew.kung@ucsf.edu>
2018-01-26 23:19:35 -0800

    Cleaned up test_algs.py
    Fixed typo and conditions in test_algs.py

--- test/test_algs.py
+++ test/test_algs.py
@@ -2,22 +2,11 @@ import numpy as np
 from example import algs
 
 def test_bubblesort():
-    # Actually test bubblesort here. It might be useful to think about
-    # some edge cases for your code, where it might fail. Some things to
-    # think about: (1) does your code handle 0-element arrays without
-    # failing, (2) does your code handle characters?
-
     x = np.array([1,2,4,0,1])
-    
-    # for now, just attempt to call the bubblesort function, should
-    # actually check output
-    sorted, assign, cond = algs.bubblesort(x))
+    sorted, assign, cond = algs.bubblesort(x)
     assert sorted = x.sort()
 
 def test_quicksort():
-
     x = np.array([1,2,4,0,1])
-    # for now, just attempt to call the quicksort function, should
-    # actually check output
     sorted, assign, cond = algs.bubblesort(x,0,len(x)-1,0,0))
     assert sorted = x.sort()
